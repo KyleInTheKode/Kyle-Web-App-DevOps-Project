3c5e7db343806f1677402b1cdf79c0fe63e0ec0f
Kyle Marsh-Dougherty <153864737+KyleInTheKode@users.noreply.github.com>
2024-02-15 17:45:48 +0000

    Added main terraform file for aks

--- aks-terraform/aks-cluster-module/main.tf
+++ aks-terraform/aks-cluster-module/main.tf
@@ -1,12 +1,20 @@
-terraform {
-  required_providers {
-    azurem = {
-        source = "hashicorp/azurerm"
-        version = "=3.0.0"
-    }
+resource "azurerm_kubernetes_cluster" "name" {
+  name = var.aks_cluster_name
+  location = var.cluster_location
+  resource_group_name = var.networking_resource_group_name
+  dns_prefix = var.dns_prefix
+  kubernetes_version = var.kubernetes_version
+  default_node_pool {
+    name = "default"
+    node_count = 1
+    vm_size = "Standard_DS2_v2"
+    enable_auto_scaling = true
+    min_count = 1
+    max_count = 1
   }
-}
 
-provider "azurem" {
-  features{}
+  service_principal {
+    client_id = var.service_principal_client_id
+    client_secret = var.service_principal_secret
+  }
 }
