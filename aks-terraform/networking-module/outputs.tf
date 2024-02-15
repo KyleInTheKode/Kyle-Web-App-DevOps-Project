9d1b685d50a11cc10d5042fa50be965c408b6121
Kyle Marsh-Dougherty <153864737+KyleInTheKode@users.noreply.github.com>
2024-02-15 14:41:08 +0000

    Added descriptions to outputs

--- aks-terraform/networking-module/outputs.tf
+++ aks-terraform/networking-module/outputs.tf
@@ -1,19 +1,24 @@
 output "vnet_id" {
+    description = "ID of the Virtual Network"
     value = azurerm_virtual_network.name.id
 }
 
 output "control_plane_subnet_id" {
+    description = "ID of the control plane subnet"
     value = azurerm_subnet.name.id
 }
 
 output "worker_node_subnet_id" {
+    description = "ID of the worker node subnet"
     value = azurerm_subnet.name2.id
 }
 
 output "networking_resource_group_name" {
+    description = "Name of the resource group"
     value = var.resource_group_name
 }
 
 output "aks_nsg_id" {
+    description = "ID of the Network Security Group"
     value = azurerm_network_security_group.name.id
 }
