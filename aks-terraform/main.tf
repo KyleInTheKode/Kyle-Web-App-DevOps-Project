d55005b7a6d01c29cc8fe4d379fdbd5b83a8eb39
Kyle Marsh-Dougherty <153864737+KyleInTheKode@users.noreply.github.com>
2024-02-15 19:48:42 +0000

    Added cluster module

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -22,3 +22,18 @@ module "networking" {
   location = "UK South"
   ip_address = ""
 }
+
+module "cluster" {
+  source = "./aks-cluster-module"
+  aks_cluster_name = "terraform-aks-cluster"
+  cluster_location = "UK South"
+  dns_prefix = "myaks-project"
+  kubernetes_version = "1.26.6"
+  service_principal_client_id = ""
+  service_principal_secret = ""
+  
+  networking_resource_group_name = module.networking.networking_resource_group_name
+  vnet_id = module.networking.vnet_id
+  control_plane_subnet_id = module.networking.control_plane_subnet_id
+  worker_node_subnet_id = module.networking.worker_node_subnet_id
+}
