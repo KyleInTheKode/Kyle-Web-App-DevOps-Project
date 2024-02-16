5b24c330ed072b0d57f2f909a8a666b0d424ce6a
Kyle Marsh-Dougherty <153864737+KyleInTheKode@users.noreply.github.com>
2024-02-16 17:25:30 +0000

    Added ID's and Secrets

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -11,8 +11,8 @@ provider "azurem" {
   features{}
   client_id       = var.client_id
   client_secret   = var.client_secret
-  #subscription_id = "your-subscription-id"
-  #tenant_id       = "your-tenant-id"
+  subscription_id = "32e2564f-55ed-48e1-b93d-d8bb039fde79"
+  tenant_id       = "47d4542c-f112-47f4-92c7-a838d8a5e8ef"
 }
 
 module "networking" {
@@ -20,7 +20,7 @@ module "networking" {
   resource_group_name = "networking-resource-group"
   vnet_address_space = ["10.0.0.0/16"]
   location = "UK South"
-  ip_address = ""
+  ip_address = var.ip_address
 }
 
 module "cluster" {
@@ -29,9 +29,9 @@ module "cluster" {
   cluster_location = "UK South"
   dns_prefix = "myaks-project"
   kubernetes_version = "1.26.6"
-  service_principal_client_id = ""
-  service_principal_secret = ""
-  
+  service_principal_client_id = var.client_id
+  service_principal_secret = var.client_secret
+
   networking_resource_group_name = module.networking.networking_resource_group_name
   vnet_id = module.networking.vnet_id
   control_plane_subnet_id = module.networking.control_plane_subnet_id
