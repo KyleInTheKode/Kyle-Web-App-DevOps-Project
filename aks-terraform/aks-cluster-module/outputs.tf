ee3873f44fac966a8fe7553492dff1b8ae5ced7a
Kyle Marsh-Dougherty <153864737+KyleInTheKode@users.noreply.github.com>
2024-02-17 03:39:11 +0000

    Fixed undelcared resources

--- aks-terraform/aks-cluster-module/outputs.tf
+++ aks-terraform/aks-cluster-module/outputs.tf
@@ -1,14 +1,14 @@
 output "aks_cluster_name" {
   description = "Name of the AKS cluster."
-  value       = azurerm_kubernetes_cluster.name.name
+  value       = azurerm_kubernetes_cluster.azurerm_kubernetes_cluster.name
 }
 
 output "aks_cluster_id" {
   description = "ID of the AKS cluster."
-  value       = azurerm_kubernetes_cluster.name.id
+  value       = azurerm_kubernetes_cluster.azurerm_kubernetes_cluster.id
 }
 
 output "aks_kubeconfig" {
   description = "Kubeconfig file for accessing the AKS cluster."
-  value       = azurerm_kubernetes_cluster.name.kube_config_raw
+  value       = azurerm_kubernetes_cluster.azurerm_kubernetes_cluster.kube_config_raw
 }
